72f350a3a9a809a237635046ad61e3ce54491f20
Harttyn Arce <harttyn.arce@gmail.com>
2020-06-27 12:24:47 -0400

    otro commit que se olvido

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,2 +1,25 @@
 public class Jugador {
+    private String nombre;
+    private String apodo;
+
+    public Jugador(String nombre, String apodo) {
+        this.nombre = nombre;
+        this.apodo = apodo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
 }
